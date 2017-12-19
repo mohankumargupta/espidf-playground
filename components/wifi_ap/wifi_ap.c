dd02f2c9486eef18a414a28e9f3dbf43123961fb
mohankumargupta <mohankumargupta@users.noreply.github.com>
2017-12-19 22:06:28 +1100

    Update wifi_ap.c

--- components/wifi_ap/wifi_ap.c
+++ components/wifi_ap/wifi_ap.c
@@ -56,14 +56,4 @@ static void wifi_ap(void)
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-void app_main()
-{
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK( ret );
 
-    wifi_ap();
-}
