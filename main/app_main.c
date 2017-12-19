138e6f58727020b59f213ff0bd6320f797a221ee
mohankumargupta <mohankumargupta@users.noreply.github.com>
2017-12-19 22:10:13 +1100

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -1,1 +1,23 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "wifi_ap.h"
+
+
+void app_main()
+{
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    wifi_ap();
+}
 
