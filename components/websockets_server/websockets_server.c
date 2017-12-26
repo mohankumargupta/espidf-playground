241a0f72bf73c80823363ef0fb7b9dc4a70d199a
mohankumargupta <mohankumargupta@users.noreply.github.com>
2017-12-26 15:06:04 +1100

    Update websockets_server.c

--- components/websockets_server/websockets_server.c
+++ components/websockets_server/websockets_server.c
@@ -23,7 +23,7 @@ void websockets_handshake(struct netconn *newconn) {
     char *data;
     uint16_t data_length;
 
-
+    const char WEBSOCKET_HEADER[] = "Upgrade: websocket";
     const char WEBSOCKET_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
     const char WEBSOCKET_KEY[] = "Sec-WebSocket-Key: ";
     const char WEBSOCKET_RSP[] = "HTTP/1.1 101 Web Socket Protocol Handshake\n" \
@@ -38,6 +38,19 @@ void websockets_handshake(struct netconn *newconn) {
     if (incoming_netbuf != NULL && err==ERR_OK) {
     	  err = netbuf_data(incoming_netbuf, (void **) &data, &data_length );
     	  if (err == ERR_OK) {
+
+    		  // Is it websockets handshake?
+    		  if ( (strstr(data,"GET") != NULL) &&  strstr(data, WEBSOCKET_HEADER) != NULL) {
+              printf("It is a websockets handshake");
+    		  }
+
+    		  // Must be websockets frame
+    		  else {
+    			  printf("It is a websockets frame");
+    			  netbuf_delete(incoming_netbuf);
+    			  return;
+    		  }
+
 
     		  // Find the start of the sec_websocket_key value in data
     		  char *sec_websocket_key_start = strstr(data, WEBSOCKET_KEY) + sizeof(WEBSOCKET_KEY) - 1;
@@ -70,12 +83,29 @@ void websockets_handshake(struct netconn *newconn) {
     		  snprintf(buf, sizeof(buf), WEBSOCKET_RSP, encoded_key);
     		  printf("Websocket handshake response:%s\n", buf);
     		  netconn_write(newconn, buf, strlen(buf), NETCONN_COPY);
+
+    		/*
+    		  buf[0] = 0x81;
+    		  buf[1] = 4;
+    		  buf[2] = 'd';
+    		  buf[3] = 'e';
+    		  buf[4] = 'a';
+    		  buf[5] = 'd';
+    		  netconn_write(newconn, buf, 6, NETCONN_COPY);
+
+    		  printf("sent data...\n");
+    		  */
+    		  printf("hopefully, receive websocket frames\n");
+    		  while (true) {
+    			  err_t err = netconn_recv(newconn, &incoming_netbuf) ;
+    		    printf("received something from websockets\n");
+    		  }
     	  }
     }
 
     netconn_close(newconn);
     netbuf_delete(incoming_netbuf);
-
+    printf("exiting...\n");
 }
 
 void websockets_callback(struct netconn *conn, enum netconn_evt event, uint16_t len) {
@@ -102,11 +132,16 @@ void websockets_server(void *pvParameters) {
     struct netconn *conn, *newconn;
 
     printf("Starting websockets server...");
+
     conn = netconn_new_with_callback(NETCONN_TCP, websockets_callback);
+    //conn = netconn_new(NETCONN_TCP);
     netconn_bind(conn, NULL, WEBSOCKETS_PORT);
     netconn_listen(conn);
+    printf("listening");
     while(netconn_accept(conn, &newconn) == ERR_OK) {
+    	  printf("something coming in...\n");
     	  websockets_handshake(newconn);
+    	  printf("going out...\n");
     }
 
     printf("Ending websockets server...");
